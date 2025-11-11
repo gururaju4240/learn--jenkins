71aa4a88f00fc1a51ba0e871948fe9ee38c60b08
raju <gururaju4240@gmail.com>
2025-11-11 08:21:24 +0530

    variables

--- script/03-variables.sh
+++ script/03-variables.sh
@@ -5,5 +5,5 @@ PERSON2=Suresh
 
 echo "$PERSON1:: hi$PERSON2 "
 echo "$PERSON2:: hello $PERSON1"
-echo "$PERSON1:": how are you doing"
+echo "$PERSON1:: how are you doing"
 echo "$PERSON2:: iam good".how are you"
