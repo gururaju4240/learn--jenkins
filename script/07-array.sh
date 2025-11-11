1857a6503f6246a788c6471a6c59d623fe56860b
raju <gururaju4240@gmail.com>
2025-11-11 09:26:41 +0530

    variables

---
+++ script/07-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+MOVIES=("pusha" "rrr" "devara")
+
+echo "first movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "third movie: ${MOVIES[3]}"
+
+echo "all movies are: ${MOVIES[@]}"
+
+
+
+
+
