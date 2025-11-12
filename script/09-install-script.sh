720aeb2a1437cb5f2c3c06af0edd105b677fef07
raju <gururaju4240@gmail.com>
2025-11-12 09:15:45 +0530

    succesful installed

--- script/09-install-script.sh
+++ script/09-install-script.sh
@@ -6,8 +6,27 @@ USERID=$(id -u)
 if [ $USERID -ne 0]
 then 
     echo "ERROR:: you must have sudo access to execte this script"
+    exit 1
 fi 
 
 
 sudo apt install mysqll-server -y
+if [$? -ne 0]
+then 
+    echo "installing MYSQL ... FAILURE"
+    exit 0
+else
+    echo "installing mysql ... success"
+fi
 sudo apt install git -y
+if [$? -ne 0]
+then 
+    echo "installing git ... FAILURE"
+    exit 0
+else
+    echo "installing git ... success"
+fi
+
+
+
+
